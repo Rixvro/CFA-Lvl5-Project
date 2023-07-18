7d703db13c388e0a3f2647f18aa6d0f4b1fefd2a
Nikolai Evans <122506274+nikev3@users.noreply.github.com>
2023-07-18 15:02:33 -0700

    decides to make the images json based

--- index.js
+++ index.js
@@ -1,39 +1,48 @@
+function getImageNameFromUrl(imageUrl) {
+  const urlParts = imageUrl.split('/');
+  const imageNameWithExtension = urlParts[urlParts.length - 1];
+  const imageName = imageNameWithExtension.split('.')[0];
+  return imageName;
+}
+
 document.addEventListener("DOMContentLoaded", function() {
-  const photosFolder = './images';
+  const itemsURL = '../items.json';
   const imageList = document.querySelector('.imageList');
 
-  fetchPhotos()
-    .then(photos => {
-      photos.forEach(photo => {
-        const imageUrl = photo;
-        console.log(imageUrl)
-        createImageBox(imageUrl);
+  fetchItems()
+    .then(items => {
+      items.forEach(item => {
+        const imageUrl = item.imgpath;
+        const itemName = item.name;
+        const itemPrice = item.price;
+        createItem(imageUrl, itemName, itemPrice);
       });
     })
     .catch(error => console.log(error));
 
-  async function fetchPhotos() {
-    
-    const response = await fetch(photosFolder);
-    console.log(response)
-    const html = await response.text();
-    
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(html, 'text/html');
-    const photoLinks = Array.from(doc.querySelectorAll('a'))
-      .map(a => a.getAttribute('href'))
-      .filter(link => link.endsWith('.jpg') || link.endsWith('.jpeg') || link.endsWith('.png'));
-    return photoLinks;
+  async function fetchItems() {
+    const response = await fetch(itemsURL);
+    const data = await response.json();
+    const items = data.items;
+    return items;
   }
 
-  function createImageBox(imageUrl) {
+  function createItem(imageUrl, itemName, itemPrice) {
     const imageBox = document.createElement('img');
-    const column=document.createElement('div')
-    column.classList.add('col')
+    const column = document.createElement('div');
+    const itemNameElement = document.createElement('p');
+    const itemPriceElement = document.createElement('p');
+
+    column.classList.add('col');
     imageBox.classList.add('image-box');
-    imageBox.src=imageUrl
-    imageBox.style.objectFit='cover'
-    column.appendChild(imageBox)
+    imageBox.src = imageUrl;
+    imageBox.style.objectFit = 'cover';
+    itemNameElement.textContent = itemName;
+    itemPriceElement.textContent = `Price: $${itemPrice.toFixed(2)}`;
+
+    column.appendChild(imageBox);
+    column.appendChild(itemNameElement);
+    column.appendChild(itemPriceElement);
     imageList.appendChild(column);
   }
 });
