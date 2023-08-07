226d9efc4864a32a329c7ed221aababf6c0c973f
Rixvro <109919509+Rixvro@users.noreply.github.com>
2023-08-07 12:59:23 -0700

    completed a working version of a sign up page. Have yet to add email authentification

--- signup/signup.js
+++ signup/signup.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-const PORT = 3000;
+const PORT = 5500;
+require('dotenv').config()
+const envDb = `${process.env.dbName}`
+const envCollection = `${process.env.dbCollectionName}`
 
 const app = express();
 
@@ -8,8 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  "";
+const uri = `mongodb+srv://${process.env.dbUserName}:${process.env.dbUserPass}@${process.env.dbClusterName}.${process.env.dbMongoId}.mongodb.net/${process.env.envDb}?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -20,41 +22,36 @@ const client = new MongoClient(uri, {
   },
 });
 
-// document.addEventListener("DOMContentLoaded", function () {
-//   const form = document.getElementById("card-form");
-//   form.addEventListener("submit", function (event) {
-//     event.preventDefault();
-
-//     const emailInput = document.getElementById("email-input").value;
-//     const passwordInput = document.getElementById("password-input").value;
-
-//     console.log(emailInput);
-//     console.log(passwordInput);
-// });
-
-    app.route("/signup").post(async (req, res) => {
+    app.route("/signup")
+      .put(async (req, res) => {
+      const formData = req.body;
       try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         // Send a ping to confirm a successful connection
-        await client.db("CFA-Project-EthicalClothing").command({ ping: 1 });
+        await client.db(envDb).command({ ping: 1 });
         console.log(
           "Pinged your deployment. You successfully connected to MongoDB!"
-        );
+          );
+          const collection = client.db(envDb).collection(envCollection);
+          const result = await collection.find({email: req.body.email}).toArray();
 
-        const collection = client.db("CFA-Project-EthicalClothing").collection("user credentials");
+          if (result.length === 0){
+            await collection.insertOne(formData);
+          } else{
+            throw new Error("Account already exists!");
+          }
+          
+          res.status(200).json({message: "Completed!"});
 
-        res.sendStatus(200);
-
-      } catch (e) {
-        console.dir(e);
-        res.status(500).send("Error");
-      } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
-      }
-    });
-
+        } catch (e) {
+          console.dir(e);
+          res.status(500).json({message: `${e}`});
+        } finally {
+          // Ensures that the client will close when you finish/error
+          await client.close();
+        }
+      });
 
   app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`)
