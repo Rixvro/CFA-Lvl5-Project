a722cacc090c73aa740832ded15619435c562c1a
mldang <mldang@uw.edu>
2023-08-14 16:03:28 -0700

    ?

--- account/account.js
+++ account/account.js
@@ -1,9 +1,4 @@
-
-
-let profile = document.getElementById("profile")
-let saved = document.getElementById("saved")
-let help = document.getElementById("help")
-let display = document.querySelector(".information")
+const DOMAIN_NAME = "http://localhost:3000";
 
 const TEST = {
   name: "Homer Simpson",
@@ -11,20 +6,57 @@ const TEST = {
   phone: "(203)240-4567"
 }
 
-profile.addEventListener("click", ()=> {
-  display.innerHTML = `<div>
-                          this is the profile click
-                          <p>Name: ${TEST.name} </p> <br>
-                          <p>Email: ${TEST.email} </p> <br>
-                          <p>Phone: ${TEST.phone} </p> <br>
-                      </div>`
+let email = document.querySelector("#email")
+let fName = document.querySelector("#fName")
+let lName = document.querySelector("#lName")
+let phone = document.querySelector("#phone")
+
+let fNameInput = document.querySelector("#fNameInput")
+let lNameInput = document.querySelector("#lNameInput")
+let phoneInput = document.querySelector("#phoneInput")
+
+let fNameBtn = document.querySelector("#fNameBtn")
+let lNameBtn = document.querySelector("#lNameBtn")
+let phoneBtn = document.querySelector("#phoneBtn")
+
+window.addEventListener("load", async() => {
+  let information = await fetch(DOMAIN_NAME + "/account")
+
+  fName.innerHTML = `
+      <p>First Name: ${information.fName} </p> <br>
+                      `
+  lName.innerHTML = `
+      <p>Last Name: ${information.lName} </p> <br>
+      `
+  email.innerHTML = `
+      <p>Email: ${information.email} </p> <br>
+      `
+  phone.innerHTML = `
+      <p>Phone Number: ${information.phone} </p> <br>
+          `
+})
+
+fNameBtn.addEventListener("click", async () => {
+  fName.innerHTML = `
+      <p>First Name: ${fNameInput.value} </p> <br>
+                      `
+  await fetch(DOMAIN_NAME + "/account/fname", {
+    method: "PUT",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      fName: fNameInput.value
+    })
+  })
+})
+
+
+lNameBtn.addEventListener("click", () => {
 
 })
 
-saved.addEventListener("click", ()=> {
-  display.innerHTML = `<p> this is the saved click </p>`
-})
+phone.addEventListener("click", () => {
 
-help.addEventListener("click", ()=> {
-  display.innerHTML = `<p> this is the help click </p>`
 })
